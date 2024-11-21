6a781db68cbab60ced3c882251819c4df8d34953
Bartosz Korszun <61890250+bartoszkorszun@users.noreply.github.com>
2024-11-21 10:35:21 +0100

    Create 06-arrays.java

---
+++ java-marathon-coderslab/06-arrays.java
@@ -0,0 +1,9 @@
+class CodersLab {
+    public static void main(String[] args) {
+        
+        int[] tab = {1, 2, 3, 4, 5};
+        for (int i = 0; i < tab.length; i++) {
+            System.out.println(tab[i]);
+        }
+    }
+}
