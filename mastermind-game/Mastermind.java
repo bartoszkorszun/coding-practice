768f457914bc42dd4ed1b4252f63602a1cfe9084
bartoszkorszun <bartoszkorszun@gmail.com>
2024-11-22 23:04:36 +0100

    mastermind game finished

--- mastermind-game/Mastermind.java
+++ mastermind-game/Mastermind.java
@@ -1,21 +1,78 @@
+import java.util.Random;
 import java.util.Scanner;
 
 class MasterMind {
     public static void main(String[] args) {
         
         System.out.println("Wellcome to MasterMind!");
-        System.out.println("GHuess the secret code of digits between 1 and 6.");
+        System.out.println("Guess the secret code of 4 digits between 1 and 6.");
 
-        try (Scanner scanner = new Scanner(System.in)) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.println("Enter your guess: ");
-            
-            String guess = scanner.nextLine();
-            System.out.println("You guessed: " + guess);
-            
-        } catch (Exception e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+        int codeLength = 4;
+        int maxDigit = 6;
+        int[] secretCode = new int[codeLength];
+        int[] userCode = new int[codeLength];
+        boolean isGuessed = false;
+
+        for (int i = 0; i < codeLength; i++) {
+            secretCode[i] = random.nextInt(maxDigit) + 1;
         }
+
+        while (!isGuessed) {
+            try {
+                System.out.println("Enter your guess: ");
+                String guess = scanner.nextLine();
+                
+                if (guess.length() != codeLength) {
+                    throw new NumberFormatException();
+                }
+
+                for (int i = 0; i < codeLength; i++) {
+                    userCode[i] = Character.getNumericValue(guess.charAt(i));
+                    if (userCode[i] < 1 || userCode[i] > maxDigit) {
+                        throw new NumberFormatException();
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println("An error occurred. Enter numbers from 1 to " + maxDigit);
+            }
+
+            int identicalNotInPlace = 0;
+            int identicalInPlace = 0;
+            boolean[] countedInUserCode = new boolean[codeLength];
+            boolean[] countedInSecretCode = new boolean[codeLength];
+
+            for (int i = 0; i < codeLength; i++) {
+                if (userCode[i] == secretCode[i]) {
+                    identicalInPlace++;
+                    countedInUserCode[i] = true;
+                    countedInSecretCode[i] = true;
+                }
+            }
+
+            for (int i = 0; i < codeLength; i++) {
+                if (!countedInUserCode[i]) {
+                    for (int j = 0; j < codeLength; j++) {
+                        if (!countedInSecretCode[j] && userCode[i] == secretCode[j]) {
+                            identicalNotInPlace++;
+                            countedInUserCode[i] = true;
+                            countedInSecretCode[j] = true;
+                        }
+                    }
+                }   
+            }
+
+            if (identicalInPlace == codeLength) {
+                System.out.println("Nice - code guessed!");
+                isGuessed = true;
+            } else {
+                System.out.println("Correct numbers on correct positions: " + identicalInPlace);
+                System.out.println("Correct numbers on wrong positions: " + identicalNotInPlace);
+            }
+        }
+
+        scanner.close();
     }
 }
