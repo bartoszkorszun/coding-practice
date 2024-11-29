aab42ea4676ab9a916b2a80225bf6e031cfae558
Bartosz Korszun <61890250+bartoszkorszun@users.noreply.github.com>
2024-11-29 11:45:58 +0100

    Create 14-arr-sum.java

---
+++ gfg-exercises/14-arr-sum.java
@@ -0,0 +1,12 @@
+class GFG {
+    public static void main(String[] args) {
+        
+        int[] arr = {2, 4, 6, 7, 9};
+
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];    
+        }
+        System.out.println(sum);
+    }
+}
