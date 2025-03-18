5c3ac12affb4acd6ffa2a0a36e882b46260f9275
Bartosz Korszun <61890250+bartoszkorszun@users.noreply.github.com>
2025-03-18 12:44:50 +0100

    Update 1119_fibonacci.java

--- algorithms/exercises/1_1/1119_fibonacci.java
+++ algorithms/exercises/1_1/1119_fibonacci.java
@@ -1,13 +1,21 @@
+import java.util.*;
+
 class ALG {
     // Fibonacci
+    private static Map<Integer, Long> memo = new HashMap<>();
+
     public static void main(String[] args){
         for (int N = 0; N < 100; N++)
             StdOut.println(N + " " + F(N)); // around 55 iterations under an hour
     }
-    // TODO write better implementation of F
-    public static long F(int N){
+    
+    private static long F(int N){
         if (N == 0) return 0;
         if (N == 1) return 1;
-        return F(N-1) + F(N-2);
+        if (memo.containsKey(N))return memo.get(N);
+
+        long result = F(N - 1) + F(N - 2);
+        memo.put(N, result);
+        return result;
     }
 }
